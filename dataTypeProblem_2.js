a3794c222222c4d3898d5e57b05448bdc0e3a779
said_waleed <saidowaleedo321@gmail.com>
2025-08-09 09:55:01 +0300

    Add files via upload

---
+++ dataTypeProblem_2.js
@@ -0,0 +1,9 @@
+    function main() {
+    let r = parseFloat(readLine());
+    const PI = Math.PI;
+    
+    console.log(PI * r * r);     
+    console.log(2 * PI * r);      
+
+    }
+    main();
